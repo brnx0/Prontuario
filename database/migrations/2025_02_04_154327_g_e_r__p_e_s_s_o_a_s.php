eb4ba72a5ce50c9fc90921fc892c39461bfbbbfb
Breno Oliveira <breno6@ba.estudante.senai.br>
2026-04-14 23:40:35 -0300

    Fix: add validate fields required before submit

--- database/migrations/2025_02_04_154327_g_e_r__p_e_s_s_o_a_s.php
+++ database/migrations/2025_02_04_154327_g_e_r__p_e_s_s_o_a_s.php
@@ -60,15 +60,15 @@ return new class extends Migration
         $table->uuid('atend_cod')->primary();
         $table->datetime('dt_atendimento');
         $table->string('situacao_queixa')->nullable();
-        $table->string('mmhg')->nullable();
-        $table->string('bpm')->nullable();
-        $table->string('rpm')->nullable();
-        $table->string('spo2')->nullable();
-        $table->string('temp')->nullable();
-        $table->string('kg')->nullable();
-        $table->string('hgt')->nullable();
-        $table->longtext('desc_caso')->nullable();
-        $table->longtext('receituario')->nullable();
+        $table->string('mmhg')->nullable()->length(8);
+        $table->string('bpm')->nullable()->length(8);
+        $table->string('rpm')->nullable()->length(8);
+        $table->string('spo2')->nullable()->length(8);
+        $table->string('temp')->nullable()->length(8);
+        $table->string('kg')->nullable()->length(8);
+        $table->string('hgt')->nullable()->length(8);
+        $table->longtext('desc_caso')->nullable()->length(2000);
+        $table->longtext('receituario')->nullable()->length(2000);
         });
 
 
